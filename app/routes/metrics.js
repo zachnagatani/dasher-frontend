13333fba730700e9333399df8b38079b35b61361
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-03 11:03:44 -0700

    Feat: Bar Chart for Issues

--- app/routes/metrics.js
+++ app/routes/metrics.js
@@ -3,7 +3,28 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return {
-            options: ['Issues', 'Sign Ups']
+            options: ['Issues', 'Sign Ups'],
+            data: [
+                {
+                    label: '12/16',
+                    value: 2
+                }, {
+                    label: '01/17',
+                    value: 2
+                }, {
+                    label: '02/17',
+                    value: 5
+                }, {
+                    label: '03/17',
+                    value: 7
+                }, {
+                    label: '04/17',
+                    value: 1
+                }, {
+                    label: '05/17',
+                    value: 0
+                }
+            ]
         };
     }
 });
