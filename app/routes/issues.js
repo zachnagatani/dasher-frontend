b7801c254d9e88f77c702780b9bedc9d3d466ec7
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-03 12:16:15 -0700

    Chore; Componentize issue detail

--- app/routes/issues.js
+++ app/routes/issues.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        let issues = this.get('store').findAll('issue');
-
         return {
             sections: ['Customer', 'Customer\'s Email', 'Description', 'Status', 'Opened At', 'Closed At', 'Employee'],
-            issues: issues
+            issues: this.get('store').findAll('issue')
         };
 
     }
