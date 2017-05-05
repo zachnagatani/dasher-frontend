83d404f0b44e4d040a724e379beb3f44f91b35de
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-04 20:26:12 -0700

    switch back to / for rooturl

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,6 @@ module.exports = function(environment) {
     modulePrefix: 'dasher',
     environment: environment,
     rootURL: '/',
-    routerRootURL: '/app/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
