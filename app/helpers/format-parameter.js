bda3e886426deec49b90a47b2736f995689cca86
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-31 16:28:21 -0700

    Remove unreachable code

--- app/helpers/format-parameter.js
+++ app/helpers/format-parameter.js
@@ -4,25 +4,18 @@ export function formatParameter([parameter]) {
   switch (parameter) {
     case 'customer_name':
       return 'Customer Name';
-      break;
     case 'customer_email':
       return 'Email';
-      break;
     case 'description':
       return 'Description';
-      break;
     case 'status:desc':
       return 'Status';
-      break;
     case 'opened_at':
       return 'Open Date';
-      break;
     case 'closed_at:desc':
       return 'Close Date';
-      break;
     case 'handling_employee':
       return 'Employee';
-      break;
   }
 }
 
